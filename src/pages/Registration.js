42fdd3fb7d48d7c012a01b1347886089432eb89c
Hung Vu <ublxf@student.kit.edu>
2020-08-29 06:53:15 +0200

    Edited Registration to use useForm hook and added basic callback function for submitting the form

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -2,6 +2,8 @@ import React from "react"
 import styled from "styled-components"
 import Input from "../components/Input"
 import Button from "../components/Button"
+import useForm from "../hooks/useForm"
+import { request } from "../api"
 
 const Container = styled.div`
     @import url('https://fonts.googleapis.com/css2?family=Merriweather&display=swap');
@@ -24,6 +26,7 @@ const RightSide = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin-top: 5vh;
 `
 
 const Form = styled.form`
@@ -44,29 +47,35 @@ const ColorContainer = styled.div`
         margin-right: 5px;
     }
 `
+async function callback(inputs) {
+    const res = await request({ method: "POST", path: "/signup", body: { ...inputs } })
+    if (res.status !== 200) return // handle error
+}
 
 const Registration = () => {
+    const initialValues = { username: "", email: "", confirmEmail: "", password: "", confirmPassword: "", bubbleColor: "#ff4f5b", textColor: "#000000" }
+    const { inputs, handleInputChange, handleSubmit } = useForm(initialValues, callback)
     return (
         <Container>
             <LeftSide imgUrl={process.env.PUBLIC_URL + "/images/registration.png"}></LeftSide>
             <RightSide>
                 <h1>Sign Up</h1>
-                <Form action="">
+                <Form onSubmit={handleSubmit}>
                     <label htmlFor="username">Username:</label>
-                    <Input type="text" name="username" placeholder="username.." />
+                    <Input type="text" name="username" placeholder="username.." value={inputs.username} onChange={handleInputChange} />
                     <label htmlFor="email">Email:</label>
-                    <Input type="text" name="email" placeholder="email.." />
+                    <Input type="text" name="email" placeholder="email.." value={inputs.email} onChange={handleInputChange} />
                     <label htmlFor="confirmEmail">Confirm Email:</label>
-                    <Input type="text" name="confirmEmail" placeholder="confirm email.." />
+                    <Input type="text" name="confirmEmail" placeholder="confirm email.." value={inputs.confirmEmail} onChange={handleInputChange} />
                     <label htmlFor="password">Password:</label>
-                    <Input type="password" name="password" placeholder="password.." />
+                    <Input type="password" name="password" placeholder="password.." value={inputs.password} onChange={handleInputChange} />
                     <label htmlFor="confirmPassword">Confirm password:</label>
-                    <Input type="password" name="confirmPassowrd" placeholder="confirm password.." />
+                    <Input type="password" name="confirmPassowrd" placeholder="confirm password.." value={inputs.confirmPassword} onChange={handleInputChange} />
                     <ColorContainer>
                         <label htmlFor="bubbleColor">Bubble Color:</label>
-                        <input type="color" name="bubbleColor" value="#ffffff" />
+                        <input type="color" name="bubbleColor" value={inputs.bubbleColor} onChange={handleInputChange} />
                         <label htmlFor="textColor">Text Color:</label>
-                        <input type="color" name="textColor" value="#ffffff" />
+                        <input type="color" name="textColor" value={inputs.textColor} onChange={handleInputChange} />
                     </ColorContainer>
                     <div style={{ marginTop: "5px" }}>
                         <Button type="submit" color="#4f64ff">Sign Up</Button>
