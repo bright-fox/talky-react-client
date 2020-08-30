5f5f88b2c201524103379bce74fbf9d67ad998d9
Hung Vu <ublxf@student.kit.edu>
2020-08-30 16:00:55 +0200

    Added default value for api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export const request = async ({ method, path, body }) => {
     const headers = { "Content-Type": "application/json" }
-    const res = await fetch(process.env.API_URL + path, { method, headers, body: JSON.stringify(body) })
+    const res = await fetch(`${process.env.API_URL || "http://localhost:4001"}${path}`, { method, headers, body: JSON.stringify(body) })
     return res
 }
