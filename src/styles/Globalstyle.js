52f59cfaf6af8562b002f8f481f8b5f3757ec6e3
Hung Vu <ublxf@student.kit.edu>
2020-09-06 10:29:45 +0200

    Fixed: issues with borders due to not using border-box

--- src/styles/Globalstyle.js
+++ src/styles/Globalstyle.js
@@ -1,9 +1,10 @@
 import { createGlobalStyle } from "styled-components"
 
 export default createGlobalStyle`
-  body {
+  * {
     padding: 0;
     margin: 0;
+    box-sizing: border-box;
   }
 
   img {
